0a99dfc9c9216a37ef13171cea7d0c311a9fdda2
eunjuchoi <eunjuzzoo@naver.com>
2024-02-02 14:04:45 +0900

    Add: Color+ 파일 추가 (#1)

---
+++ Basterdz-iOS/Basterdz-iOS/DesignSystem/Color+.swift
@@ -0,0 +1,47 @@
+//
+//  Color+.swift
+//  Basterdz-iOS
+//
+//  Created by EUNJU on 2024/02/02.
+//
+
+import SwiftUI
+
+extension Color {
+    
+    static var mainRed: Color {
+        return Color(UIColor(red: 255.0 / 255.0, green: 76.0 / 255.0, blue: 62.0 / 255.0, alpha: 1.0))
+    }
+    
+    static var mainBlack: Color {
+        return Color(UIColor(red: 31.0 / 255.0, green: 31.0 / 255.0, blue: 31.0 / 255.0, alpha: 1.0))
+    }
+    
+    static var pink1: Color {
+        return Color(UIColor(red: 255.0 / 255.0, green: 192.0 / 255.0, blue: 188.0 / 255.0, alpha: 1.0))
+    }
+    
+    static var pink2: Color {
+        return Color(UIColor(red: 255.0 / 255.0, green: 120.0 / 255.0, blue: 110.0 / 255.0, alpha: 1.0))
+    }
+    
+    static var grey1: Color {
+        return Color(UIColor(red: 245.0 / 255.0, green: 245.0 / 255.0, blue: 245.0 / 255.0, alpha: 1.0))
+    }
+    
+    static var grey2: Color {
+        return Color(UIColor(red: 182.0 / 255.0, green: 182.0 / 255.0, blue: 182.0 / 255.0, alpha: 1.0))
+    }
+    
+    static var grey3: Color {
+        return Color(UIColor(red: 129.0 / 255.0, green: 129.0 / 255.0, blue: 129.0 / 255.0, alpha: 1.0))
+    }
+    
+    static var grey4: Color {
+        return Color(UIColor(red: 109.0 / 255.0, green: 109.0 / 255.0, blue: 109.0 / 255.0, alpha: 1.0))
+    }
+    
+    static var white: Color {
+        return Color(UIColor(white: 1.0, alpha: 1.0))
+    }
+}
