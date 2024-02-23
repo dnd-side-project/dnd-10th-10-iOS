8d80e17f94ce662ba9d79229dc3c17654b2fbae9
hyun subin <sunny5875@hanyang.ac.kr>
2024-02-24 06:07:59 +0900

    Feat: GetRoomInfoUsingCode WIP (#38)

--- Basterdz-iOS/Basterdz-iOS/Infrastructure/Network/Foundation/NetworkEnvironment.swift
+++ Basterdz-iOS/Basterdz-iOS/Infrastructure/Network/Foundation/NetworkEnvironment.swift
@@ -16,7 +16,7 @@ extension NetworkEnvironment {
     enum HTTPHeaderFields {
         static let `default`: [String: String] = [
             "Content-Type": "application/json",
-            "Authorization": "eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJiYXN0ZXJkeiIsImlhdCI6MTcwODcxMTY3MywiZXhwIjoxNzA5NzEyOTgxLCJtZW1iZXJJZCI6MX0.DGPFDD3as56wXdVNrdHC7hayrvLBJdHKbWbvmhCH1hI"
+            "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJiYXN0ZXJkeiIsImlhdCI6MTcwODcxMTY3MywiZXhwIjoxNzA5NzEyOTgxLCJtZW1iZXJJZCI6MX0.DGPFDD3as56wXdVNrdHC7hayrvLBJdHKbWbvmhCH1hI"
         ]
     }
 }
